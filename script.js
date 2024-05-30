c70eecae33c5cd79c6721d8f30b2bd0992300756
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-05-30 09:28:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function stringChop(str, size) {
   // your code here
+	if (str === null) {
+    return [];
+  }
+
+  const result = [];
+  const strLength = str.length;
+
+  for (let i = 0; i < strLength; i += size) {
+    result.push(str.slice(i, i + size));
+  }
+
+  return result;
 }
 
 // Do not change the code below
